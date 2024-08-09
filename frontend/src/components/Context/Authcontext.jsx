7ed95f0a457bb4a50cc8544b2f7853a49ad990ad
ousman <ousmanmuhammed137@gmail.com>
2024-08-09 00:06:43 -0800

    additional state added to maintain token when  page refreshes

--- frontend/src/components/Context/Authcontext.jsx
+++ frontend/src/components/Context/Authcontext.jsx
@@ -1,34 +1,42 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import getuser from "../Utility/decode";
+
 const Authcontext = createContext();
+
 const useAuth = () => {
-	return useContext(Authcontext);
+  return useContext(Authcontext);
 };
+
 const AuthcontextProvider = ({ children }) => {
-	const [islogged, setIslogged] = useState(false);
-	const [role, setRole] = useState(null);
-	const [currentuser, setCurrentser] = useState(null);
-	//no need to setrole state because the role is on the current user ie it returned as a response
-	const value = { islogged, role, setIslogged, currentuser };
-	useEffect(() => {
-		const fetchUser = async () => {
-			try {
-				const response = await getuser();
-				console.log("Logged user:", response);
-				if (response.token) {
-					setIslogged(true);
-					setRole(response.role);
-				}
-				setCurrentser(response);
-			} catch (error) {
-				console.error("Error fetching user:", error);
-			}
-		};
+  const [islogged, setIslogged] = useState(false);
+  const [role, setRole] = useState(null);
+  const [currentuser, setCurrentser] = useState(null);
+  const [loading, setLoading] = useState(true); 
 
-		fetchUser();
-	}, []);
+  const value = { islogged, role, setIslogged, currentuser,loading };
 
-	return <Authcontext.Provider value={value}>{children}</Authcontext.Provider>;
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await getuser();
+        console.log("Logged user:", response);
+        if (response.token) {
+          setIslogged(true);
+          setRole(response.role);
+          setCurrentser(response);
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+      finally {
+        setLoading(false); // Set loading to false after fetching
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return <Authcontext.Provider value={value}>{children}</Authcontext.Provider>;
 };
 
 export { Authcontext, useAuth, AuthcontextProvider };
