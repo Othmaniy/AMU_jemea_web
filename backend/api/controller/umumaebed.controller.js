4edcc77d5afb5d6b874b54d5880ea4da92e1c39b
ousman <ousmanmuhammed137@gmail.com>
2024-08-14 17:22:55 -0800

    own member registration backend done

--- backend/api/controller/umumaebed.controller.js
+++ backend/api/controller/umumaebed.controller.js
@@ -1,6 +1,31 @@
 const pool = require("../../db.config");
-const { registerMemberService } = require("../service/umumaebed.service");
+const { registerMemberService, OwnMemberregisterService } = require("../service/umumaebed.service");
 
+
+const OwnMemberregister = (req,res)=>{
+	console.log("umaebed");
+	console.log(typeof(amount));
+	const amount=req.body.amount;
+	OwnMemberregisterService(amount,req.userId,(err,results)=>{
+		if(err){
+			return res
+			.status(500)
+			.json({
+				error:err,
+				message:"db connection error"
+			})
+		}
+		return res
+		.status(200)
+		.json({
+			message:"sucessfully registered"
+		})
+	})
+
+
+}
+
+//admin register member
 const registerMember = (req, res) => {
 	const { name, lastname, idnumber, phone, batch, monthlyPayment } = req.body;
 	if (!name || !lastname || !idnumber || !phone || !monthlyPayment || !batch) {
@@ -21,10 +46,12 @@ const registerMember = (req, res) => {
 	});
 };
 const getMembers = (req, res) => {
-	const sql = "SELECT * FROM umumaebed";
+	const sql = "SELECT m.*,u.name,lastname,phone,id_number FROM umumaebedmembers as m join usertable as u on (m.user_id=u.id) ";
 	pool.query(sql, (err, results) => {
 		if (err) {
-			return res.status(500).json({ message: "db connection error" });
+			return res.status(500).json({ 
+				error:err,
+				message: "db connection error" });
 		}
 		return res.status(200).json({
 			data: results,
@@ -32,43 +59,61 @@ const getMembers = (req, res) => {
 	});
 };
 
-const updateMember = (req, res) => {
-	const memberId = parseInt(req.params.id);
-	const fieldsToUpdate = [];
-	const values = [];
-	if (req.body.name) {
-		fieldsToUpdate.push("name=?");
-		values.push(req.body.name);
-	}
-	if (req.body.lastname) {
-		fieldsToUpdate.push("lastname=?");
-		values.push(req.body.lastname);
-	}
-	if (req.body.idnumber) {
-		fieldsToUpdate.push("id_number");
-		values.push(req.body.idnumber);
-	}
-	if (req.body.phone) {
-		fieldsToUpdate.push("phone");
-		values.push(req.body.phone);
-	}
-	if (req.body.monthlyPayment) {
-		fieldsToUpdate.push("monthlypayment");
-		values.push(req.body.monthlyPayment);
-	}
-	const sql = `UPDATE umumaebed SET ${fieldsToUpdate.join(", ")} WHERE id = ?`;
-	values.push(memberId);
-	pool.query(sql, values, (err, results) => {
-		if (err) {
-			return res.status(500).json({
-				error: err,
-				message: "db connection error",
-			});
+// const updateMember = (req, res) => {
+// 	const memberId = parseInt(req.params.id);
+// 	const fieldsToUpdate = [];
+// 	const values = [];
+// 	if (req.body.name) {
+// 		fieldsToUpdate.push("name=?");
+// 		values.push(req.body.name);
+// 	}
+// 	if (req.body.lastname) {
+// 		fieldsToUpdate.push("lastname=?");
+// 		values.push(req.body.lastname);
+// 	}
+// 	if (req.body.idnumber) {
+// 		fieldsToUpdate.push("id_number");
+// 		values.push(req.body.idnumber);
+// 	}
+// 	if (req.body.phone) {
+// 		fieldsToUpdate.push("phone");
+// 		values.push(req.body.phone);
+// 	}
+// 	if (req.body.monthlyPayment) {
+// 		fieldsToUpdate.push("monthlypayment");
+// 		values.push(req.body.monthlyPayment);
+// 	}
+// 	const sql = `UPDATE umumaebed SET ${fieldsToUpdate.join(", ")} WHERE id = ?`;
+// 	values.push(memberId);
+// 	pool.query(sql, values, (err, results) => {
+// 		if (err) {
+// 			return res.status(500).json({
+// 				error: err,
+// 				message: "db connection error",
+// 			});
+// 		}
+// 		return res.status(200).json({
+// 			message: "member updated sucessfully",
+// 			data: results,
+// 		});
+// 	});
+// };
+const deleteMember=(req,res)=>{
+	const memberId =parseInt(req.params.id);
+	const sql =`DELETE FROM umumaebedmembers WHERE id =?`
+	pool.query(sql,(err,results)=>{
+		if(err){
+			return res
+			.status(500)
+			.json({
+				message:"db connection error"
+			})
 		}
-		return res.status(200).json({
-			message: "member updated sucessfully",
-			data: results,
-		});
-	});
-};
-module.exports = { registerMember, getMembers, updateMember };
+		return res.
+		status(200)
+		.json({message:"user deleted successfully"})
+		
+	})
+
+}
+module.exports = { registerMember, getMembers,OwnMemberregister };
--- backend/api/router/umumaebed.router.js
+++ backend/api/router/umumaebed.router.js
@@ -2,14 +2,16 @@ const express = require("express");
 const {
 	registerMember,
 	getMembers,
-	updateMember,
+	OwnMemberregister,
 } = require("../controller/umumaebed.controller");
+const { verifytoken } = require("../middleware/verifytoken");
 
 const router = express.Router();
 
-router.post("/registerMember", registerMember);
+router.post("/ownmemberregister",verifytoken, OwnMemberregister);
+router.post("/adminregistermemebr", registerMember);
 router.get("/findmembers", getMembers);
-router.patch("/updatemember/:id", updateMember);
+// router.patch("/updatemember/:id", updateMember);
 // router.delete("/deletemember/:id",deleteMember)
 
 module.exports = router;
