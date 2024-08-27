d4d0e16fbe9ed6e3364ff236495e2abc29d74dde
ousman <ousmanmuhammed137@gmail.com>
2024-08-27 02:45:19 -0800

    routers midified to handle middlewares for file upload

--- backend/api/router/daewaandIrshad.router.js
+++ backend/api/router/daewaandIrshad.router.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const { addCourse, getCourses, updateCourse, deleteCourse, openCourse, registerForNewCourse, getEnrolledUsers, changeEnrollmentStatus, getOpenCourse, deleteEnrolledUser } = require("../controller/daewaandirshad.controller");
+const { addCourse, getCourses, updateCourse, deleteCourse, openCourse, registerForNewCourse, getEnrolledUsers, changeEnrollmentStatus, getOpenCourse, deleteEnrolledUser, uploadDerseFile, addDerseFile } = require("../controller/daewaandirshad.controller");
 const { verifytoken } = require("../middleware/verifytoken");
-
+const multer = require("multer");
 const router = express.Router();
+const path = require("path");
+const fs = require("fs");
+const uploadDirectory=path.join(
+    __dirname,
+	"../../../backend/derseFolders",
+)
+fs.mkdirSync(uploadDirectory, { recursive: true });
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,uploadDirectory)
+    },
+    filename:function(req,file,cb){
+        const uniqueSuffix=Date.now()
+        cb(null,uniqueSuffix + file.originalname)
+    }
+})
+const derseFileUpload=multer({storage:storage})
 router.post("/addcourse",addCourse)
 router.get('/getcourses',getCourses)
 router.put("/updatecourse/:id",updateCourse)
@@ -13,8 +30,9 @@ router.post("/registerfornewcourse/:id",verifytoken,registerForNewCourse)
 router.get("/getenrolleduser",getEnrolledUsers);
 router.put("/changeenrollment/:id",changeEnrollmentStatus)
 router.delete("/deleteenrolleduser/:id",deleteEnrolledUser)
-//to be done for file management
-// router.post("/daewandirshad/file", uploadFile);
+// file management
+router.post("/uploaddersefile",derseFileUpload.single("file"), uploadDerseFile);
+router.post("/adddersefile",addDerseFile)
 // router.delete("/daewandirshad/deletefile/:id", DeleteFile);
 
 module.exports=router
--- backend/api/router/library.router.js
+++ backend/api/router/library.router.js
@@ -4,12 +4,39 @@ const {
 	getAllBooks,
 	updateBook,
 	deleteBook,
+	uploadLibraryFile,
+	addLibraryFile,
 } = require("../controller/library.controller");
-
+const multer = require("multer");
 const router = express.Router();
+
+const path = require("path");
+const fs = require("fs");
+const uploadDirectory=path.join(
+	__dirname,
+	"../../../backend/libaryfiles",
+)
+fs.mkdirSync(uploadDirectory, { recursive: true });
+const storage=multer.diskStorage({
+	destination:function(req,file,cb){
+		cb(null,uploadDirectory)
+	},
+	filename:function(req,file,cb){
+		const uniqueSuffix=Date.now()
+		cb(null,uniqueSuffix + file.originalname)
+	}
+})
+
+
+const libraryFileUpload=multer({storage:storage})
 router.post("/addnewbook", addNewBook),
 	router.get("/getallbooks", getAllBooks),
 	router.patch("/updatebook/:id", updateBook),
 	router.delete("/deletebook/:id", deleteBook);
 
+	//library file management
+	//uploading in storage and returning filename
+	router.post("/uploadlibraryfile",libraryFileUpload.single("file"),uploadLibraryFile)
+	//inserting url and other description in to the database
+	router.post('/addlibraryfile',addLibraryFile)
 module.exports = router;
