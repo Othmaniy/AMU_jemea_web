772723dd3a0d3399205f7de47b6e7a2019f979b7
ousman <ousmanmuhammed137@gmail.com>
2024-10-21 23:30:25 -0800

    footer done

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,170 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<>
+			<footer
+				className="footer dark-bg position-relative mt-0"
+			>
+				{/* <div className="footer-contact ">
+					<div className="container">
+						<div className="row">
+							<div className="col">
+								<div className="theme-bg">
+									<div className="row g-0">
+										<div className="col-md-4 mb-5 mb-md-0">
+											<div className="px-5 pb-5 text-center footer-media">
+												{" "}
+												<i className="flaticon-paper-plane"></i>
+												<span>Address:</span>
+												<p className="mb-0">423B, Road Wordwide Country, USA</p>
+											</div>
+										</div>
+										<div className="col-md-4 mb-5 mb-md-0">
+											<div className="px-5 pb-5 text-center footer-media">
+												{" "}
+												<i className="flaticon-phone-call"></i>
+												<span>Phone:</span>
+												<a href="tel:+912345678900">+91-234-567-8900</a>
+											</div>
+										</div>
+										<div className="col-md-4">
+											<div className="px-5 pb-5 text-center footer-media border-0">
+												{" "}
+												<i className="flaticon-message"></i>
+												<span>Email:</span>
+												<a href="mailto:themeht23@gmail.com">
+													themeht23@gmail.com
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> */}
+				<div className="primary-footer">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-4 col-md-6 mt-5 mt-md-0 footer-list">
+								<h5>
+									quick <span className="text-theme"> links </span>
+								</h5>
+								<ul className="list-unstyled">
+									<li>
+										<a href="chemical-research.html">
+											<i className="fas fa-angle-right"></i> library see
+											available books
+										</a>
+									</li>
+									<li>
+										<a href="energy-%26-power-engineering.html">
+											<i className="fas fa-angle-right"></i>Library get files
+										</a>
+									</li>
+									<li>
+										<a href="petroleum-and-gas.html">
+											<i className="fas fa-angle-right"></i> Academi get files
+										</a>
+									</li>
+									<li>
+										<a href="agriculture-engineering.html">
+											<i className="fas fa-angle-right"></i>enroll for derses
+										</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-lg-4 col-md-6 mt-5 mt-md-0 footer-list">
+								<h5>
+									quick <span className="text-theme"> links </span>
+								</h5>
+								<ul className="list-unstyled">
+									<li>
+										<a href="civil-engineering.html">
+											<i className="fas fa-angle-right"></i>umumaebed homepage
+										</a>
+									</li>
+									<li>
+										<a href="civil-engineering.html">
+											<i className="fas fa-angle-right"></i>umumaebed register
+											for monthly payment
+										</a>
+									</li>
+									<li>
+										<a href="civil-engineering.html">
+											<i className="fas fa-angle-right"></i>get derse softcopy
+											files
+										</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-lg-4 col-md-6 mt-5 mt-lg-0">
+								<p>join us</p>
+
+								<div className="social-icons social-colored mt-3">
+									<ul className="list-inline mb-0">
+										<li className="social-facebook">
+											<a href="#">
+												<i className="fab fa-facebook-f" aria-hidden="true"></i>
+											</a>
+										</li>
+										<li className="social-twitter">
+											<a href="#">
+												<i className="fab fa-twitter" aria-hidden="true"></i>
+											</a>
+										</li>
+										<li className="social-gplus">
+											<a href="#">
+												<i
+													className="fab fa-google-plus-g"
+													aria-hidden="true"
+												></i>
+											</a>
+										</li>
+										<li className="social-linkedin">
+											<a href="#">
+												<i
+													className="fab fa-linkedin-in"
+													aria-hidden="true"
+												></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="secondary-footer">
+					<div className="container">
+						<div className="copyright">
+							<div className="row align-items-center">
+								<div className="col-md-7">
+									<span>developed by ousman muhammed</span>
+								</div>
+								<div className="col-md-5 text-md-end mt-3 mt-md-0 footer-list">
+									<ul className="list-inline">
+										<li>
+											<a href="">linkedin</a>
+										</li>
+										<li>
+											{" "}
+											<a href="">instagram</a>
+										</li>
+										<li>
+											{" "}
+											<a href="">telegram</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
